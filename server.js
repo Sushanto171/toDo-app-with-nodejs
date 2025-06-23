68bf491a24a18206915722fae2d0069f45dc77e3
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-06-23 15:56:55 +0600

    make get single togo

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const path = require("path");
 const fileName = path.join(__dirname, "db/todo.json");
 
 const server = http.createServer((req, res) => {
-  // res.end("Hello world");
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const pathName = url.pathname;
   // get all todos
-  if ((req.url === "/todos") & (req.method === "GET")) {
+  if ((pathName === "/todos") & (req.method === "GET")) {
     fs.readFile(fileName, { encoding: "utf-8" }, (err, data) => {
       if (err) throw err;
       // console.log(data);
@@ -20,31 +21,52 @@ const server = http.createServer((req, res) => {
     });
   }
   // create a todo
-  else if ((req.url === "/todos/create-todo") & (req.method === "POST")) {
-    // console.log(data);
+  else if ((pathName === "/todos/create-todo") & (req.method === "POST")) {
     let totalChunks = "";
     req.on("data", (chunk) => {
       totalChunks += chunk;
-      // console.log(chunk.toString());
     });
     req.on("end", () => {
       fs.readFile(fileName, { encoding: "utf-8" }, (err, data) => {
         if (err) throw err;
-        const dbTodos = JSON.parse(data);
         const bodyData = JSON.parse(totalChunks);
-        bodyData.createAt = new Date().toLocaleString();
+        const createdAt = new Date().toLocaleString();
+        bodyData.createdAt = createdAt;
+        const dbTodos = JSON.parse(data);
         dbTodos.push(bodyData);
-        fs.writeFile(fileName, JSON.stringify(dbTodos, null, 2), { encoding: "utf-8" }, (err) => {
-          if (err) throw err;
+        fs.writeFile(
+          fileName,
+          JSON.stringify(dbTodos, null, 2),
+          { encoding: "utf-8" },
+          (err) => {
+            if (err) throw err;
+          }
+        );
+        res.writeHead(200, {
+          message: "Fetched All todos",
+          "content-type": "application/json",
+          // "content-type": "text/html",
         });
+        res.end(JSON.stringify(bodyData));
       });
-      res.writeHead(200, {
-        message: "Fetched All todos",
-        "content-type": "application/json",
-        // "content-type": "text/html",
-      });
-      res.end(totalChunks);
     });
+  }
+  //get  a todo
+  else if ((pathName === "/todo") & (req.method === "GET")) {
+    const param = url.searchParams.get("title");
+    // console.log(param);
+
+    fs.readFile(fileName, { encoding: "utf-8" }, (err, data) => {
+      if (err) throw err;
+
+      const allTodos = JSON.parse(data);
+      const queryTodo = allTodos.filter((todo) => todo.title === param);
+
+      // console.log(queryTodo);
+      res.end(JSON.stringify(queryTodo));
+    });
+  } else {
+    res.end("Route Not Found");
   }
 });
 
